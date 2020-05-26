e43cb14b5a29eae7b3d5c83b3b44cc4061ad3e31
SavostinIllia <savostin.illia@gmail.com>
2020-05-26 12:59:22 +0200

    Error corrects

--- src/js/modules/pagePreloader.js
+++ src/js/modules/pagePreloader.js
@@ -1,18 +1,18 @@
 
 function pagePreloader(){ 
     // PRELOADER
-    // const preloader = document.querySelector('#preloader')
-    // $('body').css({
-    //     overflowY: 'hidden'
-    // })
-    // preloader.addEventListener('animationend', () => {
-    //     setTimeout(()=>{
-    //         preloader.classList.add('hide')
-    //         $('body').css({
-    //             overflowY: 'auto'
-    //         })
-    //     }, 1000)
-    // });
+    const preloader = document.querySelector('#preloader')
+    $('body').css({
+        overflowY: 'hidden'
+    })
+    preloader.addEventListener('animationend', () => {
+        setTimeout(()=>{
+            preloader.classList.add('hide')
+            $('body').css({
+                overflowY: 'auto'
+            })
+        }, 1000)
+    });
     // PRELOADER
 }
 
